f04bb064f1a4338c3c20d070731e4fc7c096c513
Sophie Archibald <arc21004@byui.edu>
2025-01-25 01:27:35 -0700

    Finished viewHandler function.

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -16,3 +16,31 @@ function handleResize() {
 }
 
 window.addEventListener("resize", handleResize);
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+    const element = event.target;
+    const source = element.getAttribute("src");
+    const photoName = source.split("-");
+    fileName = photoName[0].concat("-full.jpeg");
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(fileName, "photo"));
+
+    const viewerButton = document.querySelector(".close-viewer");
+    viewerButton.addEventListener("click", closeViewer);
+}
+
+function closeViewer() {
+    console.log("running");
+    const viewerDiv = document.querySelector(".viewer");
+    viewerDiv.remove();
+}
+
+const openGallery = document.querySelector(".gallery");
+openGallery.addEventListener("click", viewHandler);
+
