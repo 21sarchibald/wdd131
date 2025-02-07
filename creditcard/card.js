a3599cc48da52caff62fa7870aa845e083bf0794
Sophie Archibald <arc21004@byui.edu>
2025-02-07 12:25:54 -0700

    Validated card number input

--- creditcard/card.js
+++ creditcard/card.js
@@ -1,3 +1,34 @@
-const form = document.querySelector("#card-form");
+function validateNumber(number) {
+    return number == "1234123412341234";
+}
+
+function displayErrorMessage(message) {
+    document.querySelector("#errorMessage").textContent = message;
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    let errorMessage = "";
+    displayErrorMessage(errorMessage);
+
+    console.log(Number(this.cardNumber.value));
+
+    if (isNaN(this.cardNumber.value)) {
+        errorMessage = "Entered card number is not valid.";
+    }
+
+    else if (!validateNumber(this.cardNumber.value)) {
+        errorMessage = "Entered card number is not valid.";
+    }
+
+    if (errorMessage != "") {
+        displayErrorMessage(errorMessage);
+        return false;
+    }
+    return true;
+}
+
+const form = document.querySelector("#cardForm");
 
 form.addEventListener("submit", submitHandler);
