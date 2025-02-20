3f4fdcf614414e6f3664432611796ff27f01617e
Sophie Archibald <arc21004@byui.edu>
2025-02-20 00:46:21 -0700

    Created addEntry function which adds one entry to the main page. Will need updating.

--- fitness/fitness.js
+++ fitness/fitness.js
@@ -8,6 +8,36 @@ function toggleSideBar() {
 }
 
 
-
-
 document.querySelector("#sideBarButton").addEventListener("click", toggleSideBar);
+
+function addEntry() {
+
+
+    let entries = document.querySelector(".entries");
+
+    let date = document.querySelector("#dateInput").value;
+    let type = document.querySelector("#typeInput").value;
+    let reps = document.querySelector("#repsInput").value;
+    let heartRate = document.querySelector("#heartRateInput").value;
+    let duration = document.querySelector("#durationInput").value;
+
+    entries.innerHTML += `
+                <div class="exerciseEntry">
+                    <h2>Date: ${date}</h2>
+                    <p class="type">Exercise Type: ${type}</p>
+                    <p class="reps">Reps: ${reps}</p>
+                    <p class="heartRate">Heart Rate: ${heartRate} bpm</p>
+                    <p class="duration">Duration: ${duration} minutes</p>
+                </div>  
+    `
+
+console.log("working");
+}
+
+// document.querySelector("#newEntry").addEventListener("submit", addEntry);
+
+document.querySelector("#newEntry").addEventListener("submit", function(event)
+{
+    event.preventDefault();
+    addEntry();
+});
