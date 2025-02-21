5bac413f381aa5edcc3bc20c69a7b25915d07c11
Sophie Archibald <arc21004@byui.edu>
2025-02-21 14:12:41 -0700

    Created new javascript file for food page

--- fitness/food.js
+++ fitness/food.js
@@ -10,40 +10,6 @@ function toggleSideBar() {
 
 document.querySelector("#sideBarButton").addEventListener("click", toggleSideBar);
 
-let currentExerciseEntries = [
-    {
-        date: "02/16/2025",
-        type: "Running",
-        reps: "N/A",
-        heartRate: "164",
-        duration: "20"
-    },
-    {
-        date: "02/17/2025",
-        type: "Boxing",
-        reps: "N/A",
-        heartRate: "164",
-        duration: "60"
-    },
-    {
-        date: "02/18/2025",
-        type: "Cycling",
-        reps: "N/A",
-        heartRate: "168",
-        duration: "50"
-    },
-    {
-        date: "02/19/2025",
-        type: "Situps",
-        reps: "100",
-        heartRate: "144",
-        duration: "15"
-    } 
-]
-
-setLocalStorage("currentExerciseEntries", currentExerciseEntries);
-
-console.log(getLocalStorage("currentExerciseEntries"));
 
 let currentFoodEntries = {
 
@@ -82,67 +48,11 @@ function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
-
-const pastExerciseEntries = document.querySelector("#pastExerciseEntries");
-
-function addExerciseEntry() {
-    console.log("run");
-    currentExerciseEntries.push(
-        {
-            date: document.querySelector("#dateInput").value,
-            type: document.querySelector("#typeInput").value,
-            reps: document.querySelector("#repsInput").value,
-            heartRate: document.querySelector("#heartRateInput").value,
-            duration: document.querySelector("#durationInput").value
-        }
-    )
-    setLocalStorage("currentExerciseEntries", currentExerciseEntries);
-    console.log(getLocalStorage("currentExerciseEntries"));
-    convertToExerciseHtml();
-}
-
-// document.querySelector("#newExerciseEntry").addEventListener("submit", addExerciseEntry);
-
-
-function convertToExerciseHtml() {
-
-    pastExerciseEntries.innerHTML = "";
-
-    let entryList = getLocalStorage("currentExerciseEntries");
-    
-    for (let i = entryList.length - 1; i >= 0; i--) {
-       entry = entryList[i];
-
-       pastExerciseEntries.innerHTML +=
-         `
-            <div class="entry">
-                <h2>Date: ${entry.date}</h2>
-                <p class="type">Exercise Type: ${entry.type}</p>
-                <p class="reps">Reps: ${entry.reps}</p>
-                <p class="heartRate">Heart Rate: ${entry.heartRate} bpm</p>
-                <p class="duration">Duration: ${entry.duration} minutes</p>
-            </div>
-        `
-    }   
-}   
-
-convertToExerciseHtml();
-
-const exerciseEntryForm = document.querySelector("#newExerciseEntry");
-
-exerciseEntryForm.addEventListener("submit", function(event)
-{
-    console.log("works");
-    event.preventDefault();
-    addExerciseEntry();
-    exerciseEntryForm.reset();
-});
-
 function addFoodEntry() {
     console.log("run");
 
     date = document.querySelector("#foodDateInput").value;
-    entry = currentExerciseEntries[date];
+    entry = currentFoodEntries[date];
 
     if (date in currentFoodEntries) {
         currentFoodEntries[date] =
@@ -167,8 +77,11 @@ function addFoodEntry() {
     // convertToExerciseHtml();
 }
 
-document.querySelector("#newFoodEntry").addEventListener("submit", function(event)
+const foodEntryForm = document.querySelector("#newFoodEntry");
+
+foodEntryForm.addEventListener("submit", function(event)
 {
     event.preventDefault();
     addFoodEntry();
+    foodEntryForm.reset();
 })
